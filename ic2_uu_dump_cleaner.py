029f11789d1ec8c3b91deb6da0187162d1acb1d1
IronBatman2715 <72224765+IronBatman2715@users.noreply.github.com>
2022-06-16 20:45:13 -0400

    Rewrite IC2 dump cleaner in Python

---
+++ ic2_uu_dump_cleaner.py
@@ -0,0 +1,44 @@
+#  IC2 uu dump cleaner
+from os.path import exists
+import re
+
+base_str = "ic2_uu_dump"
+orig_file_name = f"{base_str}.txt"
+clean_file_name = f"{base_str}_cleaned.txt"
+
+# Open original file
+if not exists(orig_file_name):
+    raise Exception(
+        f"Original IC2 uu dump file has not been created! Should be named \"{orig_file_name}\"")
+
+orig = open(orig_file_name)
+# print(f"Opened original data from: {orig_file_name}")
+
+# Open new file
+clean = open(clean_file_name, "w")
+# print(f"Writing cleaned data to: {clean_file_name}\n")
+
+# As long as you're getting out text, keep reading
+while True:
+    line = orig.readline()
+
+    # Exit loop if no more lines
+    if not line:
+        # print("\nFinished reading original data")
+        break
+
+    # Remove logger info if applicable
+    new_line = re.sub(
+        r"^\[\d{2}:\d{2}:\d{2}\] \[Client thread\/INFO\]: \[CHAT\]\s*", "", line)
+
+    # Do not include lines that include "Infinity"
+    if not "Infinity".lower() in line.lower():
+        # Add new_line to cleaned file
+        clean.write(new_line)
+    # else:
+    #     print("Removed \"%s\"" % new_line.replace("\n", ""))
+
+# Close files
+orig.close()
+clean.close()
+# print("Done!")
