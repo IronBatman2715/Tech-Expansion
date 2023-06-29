7cedbf53aa5b48580639639b2c04a69dfcd1b195
IronBatman2715 <72224765+IronBatman2715@users.noreply.github.com>
2023-06-29 15:57:26 -0400

    Reformat ic2_uu_dump_cleaner.py

--- ic2_uu_dump_cleaner.py
+++ ic2_uu_dump_cleaner.py
@@ -1,59 +1,59 @@
 #  IC2 uu dump cleaner
-import os
-import sys
-import re
 
-base_str = "ic2_uu_dump"
-orig_file_name = f"{base_str}.txt"
-clean_file_name = f"{base_str}_cleaned.txt"
+def main():
+    import os
+    import sys
+    import re
 
-# Check original file
-if not os.path.exists(orig_file_name):
-    raise Exception(
-        f"Original IC2 uu dump file has not been created! Should be named \"{orig_file_name}\"")
+    base_str = "ic2_uu_dump"
+    orig_file_name = f"{base_str}.txt"
+    clean_file_name = f"{base_str}_cleaned.txt"
 
-# Check clean file
-if os.path.exists(clean_file_name):
-    user_input = input(
-        f"Already generated clean IC2 uu dump file! Overwrite existing {clean_file_name}? (Y/n): ")
+    # Check original file
+    if not os.path.exists(orig_file_name):
+        raise Exception(
+            f"Original IC2 uu dump file has not been created! Should be named \"{orig_file_name}\"")
 
-    if user_input == "" or user_input.lower() == "y":
-        print(f"Overwriting {clean_file_name}")
-        os.remove(clean_file_name)
-    elif user_input.lower() == "n":
-        print("Exiting program")
-        sys.exit(0)
-    else:
-        raise Exception("Invalid selection")
+    # Check clean file
+    if os.path.exists(clean_file_name):
+        while True:
+            user_input = input(
+                f"Already generated clean IC2 uu dump file! Overwrite existing {clean_file_name}? (Y/n): ").lower()
 
-orig = open(orig_file_name)
-# print(f"Opened original data from: {orig_file_name}")
+            if user_input == "" or user_input == "y":
+                print(f"Overwriting {clean_file_name}")
+                os.remove(clean_file_name)
+                break
+            elif user_input == "n":
+                print("Exiting program")
+                sys.exit(0)
+            else:
+                print("Invalid selection. Showing prompt again")
 
-# Open new file
-clean = open(clean_file_name, "w")
-# print(f"Writing cleaned data to: {clean_file_name}\n")
+    # print(f"Opening original data from: {orig_file_name}")
+    # print(f"Writing cleaned data to: {clean_file_name}\n")
+    with open(orig_file_name) as orig, open(clean_file_name, "w") as clean:
+        # As long as you're getting out text, keep reading
+        while True:
+            line = orig.readline()
 
-# As long as you're getting out text, keep reading
-while True:
-    line = orig.readline()
+            # Exit loop if no more lines
+            if not line:
+                # print("\nFinished reading original data")
+                break
 
-    # Exit loop if no more lines
-    if not line:
-        # print("\nFinished reading original data")
-        break
+            # Remove logger info if applicable
+            new_line = re.sub(
+                r"^\[\d{2}:\d{2}:\d{2}\] \[Client thread\/INFO\]: \[CHAT\]\s*", "", line)
 
-    # Remove logger info if applicable
-    new_line = re.sub(
-        r"^\[\d{2}:\d{2}:\d{2}\] \[Client thread\/INFO\]: \[CHAT\]\s*", "", line)
+            # Do not include lines that include "Infinity"
+            if not "Infinity".lower() in line.lower():
+                # Add new_line to cleaned file
+                clean.write(new_line)
+            # else:
+            #     print("Removed \"%s\"" % new_line.replace("\n", ""))
 
-    # Do not include lines that include "Infinity"
-    if not "Infinity".lower() in line.lower():
-        # Add new_line to cleaned file
-        clean.write(new_line)
-    # else:
-    #     print("Removed \"%s\"" % new_line.replace("\n", ""))
+    # print("Done!")
 
-# Close files
-orig.close()
-clean.close()
-# print("Done!")
+if __name__ == "__main__":
+    main()
